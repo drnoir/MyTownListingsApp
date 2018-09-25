a4e815da49ea07d1db40efd33aacb312a357f18e
Chris Godber <chrispgodber@gmail.com>
2018-09-25 13:36:00 +0100

    added fatfree PHP / converting html to PHP
    added framework

--- index.php
+++ index.php
@@ -1,30 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "admin";
-$password = "Ingrin_52";
-$dbname = "offers";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    echo "Connected failed";
-} 
-echo "Connected successfully";
-
-//display offers
-$sql = "SELECT * FROM `offers`";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<h1>OfferID: " . $row["OfferID"]. " - VendorID: " . $row["VendorID"]. " Redeem Code:" . $row["RedeemCode"]. "</h1>";
+$f3=require('lib/base.php');
+$f3->route('GET /',
+    function($f3) {
+        $f3->set('name','world');
+        $view=new View;
+		echo $view->render('html/header.html');
+		echo $view->render('html/menu.html');
+		echo $view->render('html/MapTown.html');
+		echo $view->render('html/offersView.html');
+        // Previous two lines can be shortened to:
+        // echo View::instance()->render('template.htm');
     }
-} else {
-    echo "0 results";
-}
-$conn->close();
+);
+$f3->run();
 ?>
---
+++ signup.php
@@ -0,0 +1,41 @@
+<!-- base HTML for Signup -->
+"<div class="container">
+     <div class = "row">
+          <div class = "col-md-6">
+            <h1>Vendor Signup</h1>
+            <p>Sign up as a new Vendor
+
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus fringilla libero at 
+
+            </p>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign Up as Vendor</button>
+          </div>
+
+          <div class = "col-md-6">
+            <h1>Customers Signup</h1>
+            <p>Sign up as a new customer and find out what offers MyTown can offer you!
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            </p>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign Up as customer</button>
+          </div>
+        
+       </div>   
+      <div class = "row">
+          <div class = "col-md-6">
+              <form class="form-signin">
+              <h2 class="form-signin-heading">Please sign in</h2>
+              <label for="inputEmail" class="sr-only">Email address</label>
+              <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+              <label for="inputPassword" class="sr-only">Password</label>
+              <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" value="remember-me"> Remember me
+                </label>
+              </div>
+              <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div> <!-- /container -->
